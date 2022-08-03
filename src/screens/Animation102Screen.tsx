229d5926be54197543c15ee21c7da98cecc72cb8
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-08-03 16:50:34 -0700

    Animation102 panResponder example

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View, Animated, PanResponder } from 'react-native';
+import React, { useRef } from 'react'
 
 const Animation102Screen = () => {
+
+
+    const pan = useRef(new Animated.ValueXY()).current;
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event([
+            null,
+            {
+                dx: pan.x,
+                dy: pan.y,
+            },
+        ],
+            { useNativeDriver: false }
+        ),
+        onPanResponderRelease: () => {
+            Animated.spring(
+                pan,
+                { toValue: { x: 0, y: 0 }, useNativeDriver: false }
+            ).start();
+        },
+    });
+
+
     return (
         <View style={styles.container}>
-            <View style={styles.purpuleBox}/>
+            <Animated.View {...panResponder.panHandlers} style={[styles.purpuleBox, pan.getLayout()]} />
         </View>
     )
 }
@@ -15,11 +39,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     purpuleBox: {
         backgroundColor: 'red',
         width: 150,
-        height: 150
+        height: 150,
+        borderRadius: 10
     }
 })
