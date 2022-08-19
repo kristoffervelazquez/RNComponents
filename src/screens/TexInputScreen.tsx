3f25a6e4f35f6b3a9799aa739d5db0636e54cfcd
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-08-18 19:02:54 -0700

    Keyborad Avoiding View with Scroll View

--- src/screens/TexInputScreen.tsx
+++ src/screens/TexInputScreen.tsx
@@ -1,7 +1,8 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
+import { View, Text, TextInput, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import React, { useState } from 'react'
 import HeaderTitle from '../components/HeaderTitle'
 import { styles } from '../theme/appTheme'
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 
@@ -18,15 +19,25 @@ const TexInputScreen = () => {
     }
 
     return (
-        <View style={styles.globalMargin}>
-            <HeaderTitle title='Text Input' />
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
 
-            <TextInput style={stylesScreen.inputStyle} placeholder='Ingrese su nombre' onChangeText={(text) => {onChange(text, 'name')}} autoCorrect={false} autoCapitalize='words' keyboardType='name-phone-pad'/>
-            <TextInput style={stylesScreen.inputStyle} placeholder='Ingrese su email' onChangeText={(text) => {onChange(text, 'email')}} keyboardType='email-address' />
-            <TextInput style={stylesScreen.inputStyle} placeholder='Ingrese su telefono' onChangeText={(text) => {onChange(text, 'phone')}} keyboardType='phone-pad'/>
-            
-            <Text style={styles.title}>{JSON.stringify(form, null, 4)}</Text>
-        </View>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.globalMargin}>
+
+                    <HeaderTitle title='Text Input' />
+
+                    <TextInput style={stylesScreen.inputStyle} placeholder='Ingrese su nombre' onChangeText={(text) => { onChange(text, 'name') }} autoCorrect={false} autoCapitalize='words' keyboardType='name-phone-pad' />
+                    <TextInput style={stylesScreen.inputStyle} placeholder='Ingrese su email' onChangeText={(text) => { onChange(text, 'email') }} keyboardType='email-address' />
+                    <TextInput style={stylesScreen.inputStyle} placeholder='Ingrese su telefono' onChangeText={(text) => { onChange(text, 'phone') }} keyboardType='phone-pad' />
+
+                    <Text style={styles.title}>{JSON.stringify(form, null, 4)}</Text>
+                    <Text style={styles.title}>{JSON.stringify(form, null, 4)}</Text>
+                    <Text style={styles.title}>{JSON.stringify(form, null, 4)}</Text>
+                    <View style={{ height: 100 }} />
+                </View>
+            </ScrollView>
+
+        </KeyboardAvoidingView>
     )
 }
 
