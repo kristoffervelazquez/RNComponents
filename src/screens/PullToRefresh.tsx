5a26fb4992df22934d3c8b59c7e12efc30bf69c3
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-08-20 19:15:20 -0700

    show message after loading and changed colors

--- src/screens/PullToRefresh.tsx
+++ src/screens/PullToRefresh.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, RefreshControl } from 'react-native'
+import { View, ScrollView, RefreshControl } from 'react-native'
 import React from 'react'
 import HeaderTitle from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
@@ -7,6 +7,7 @@ import { useState } from 'react';
 const PullToRefresh = () => {
 
     const [refreshing, setRefreshing] = useState(false);
+    const [data, setData] = useState<string>();
 
 
     const onRefresh = () => {
@@ -15,21 +16,33 @@ const PullToRefresh = () => {
         setTimeout(() => {
             console.log('terminamos');
             setRefreshing(false);
+            setData('Hola Mundo');
         }, 1500)
     }
 
     return (
         <ScrollView
+            // RefreshControll (Top spinner)
             refreshControl={
                 <RefreshControl
                     refreshing={refreshing}
                     onRefresh={onRefresh}
                     progressViewOffset={10}
+                    progressBackgroundColor='#5856D6'
+                    colors={['white', 'red', 'orange'] }
+                    style={{backgroundColor: '#5856D6'}}
+                    tintColor='white'
+                    title='Refreshing'
                 />
             }
         >
             <View style={styles.globalMargin}>
+                
                 <HeaderTitle title='Pull to refresh' />
+                {
+                    data && <HeaderTitle title={data} />
+                }
+
             </View>
         </ScrollView>
     )
