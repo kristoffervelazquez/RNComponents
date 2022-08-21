16c8c53255ffc03dc9f5682fbf6b1e72bf974af5
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-08-20 19:18:21 -0700

    margin top with useSafeInsets

--- src/screens/PullToRefresh.tsx
+++ src/screens/PullToRefresh.tsx
@@ -3,41 +3,44 @@ import React from 'react'
 import HeaderTitle from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 import { useState } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const PullToRefresh = () => {
 
-    const [refreshing, setRefreshing] = useState(false);
+    const { top } = useSafeAreaInsets()
+    const [isRefreshing, setIsRefreshing] = useState(false);
     const [data, setData] = useState<string>();
 
 
     const onRefresh = () => {
-        setRefreshing(true);
+        setIsRefreshing(true);
 
         setTimeout(() => {
             console.log('terminamos');
-            setRefreshing(false);
+            setIsRefreshing(false);
             setData('Hola Mundo');
         }, 1500)
     }
 
     return (
         <ScrollView
+            style={{ marginTop: isRefreshing ? top : 0 }}
             // RefreshControll (Top spinner)
             refreshControl={
                 <RefreshControl
-                    refreshing={refreshing}
+                    refreshing={isRefreshing}
                     onRefresh={onRefresh}
                     progressViewOffset={10}
                     progressBackgroundColor='#5856D6'
-                    colors={['white', 'red', 'orange'] }
-                    style={{backgroundColor: '#5856D6'}}
-                    tintColor='white'
-                    title='Refreshing'
+                    colors={['white', 'red', 'orange']}
+                    // style={{ backgroundColor: '#5856D6' }}
+                    // tintColor='white'
+                    // title='Refreshing'
                 />
             }
         >
             <View style={styles.globalMargin}>
-                
+
                 <HeaderTitle title='Pull to refresh' />
                 {
                     data && <HeaderTitle title={data} />
